65f26c76bad6c3cf3e7b2feaf27b308c06f0be11
kpengWang <417940197@qq.com>
2018-03-23 00:42:44 +0800

    重新整合配置文件以及依赖
    待优化：
    1. css热更新
    2. 细分dev和pro两种环境
    3. 出一份完善的demo

--- gulpfile.js
+++ gulpfile.js
@@ -1,257 +1,248 @@
-/*******************************
- Gulp plugins
- *******************************/
-const gulp = require('gulp'),
-    sass = require('gulp-sass'), //sass编译
-    babel = require('gulp-babel'),
-    autoprefixer = require('gulp-autoprefixer'), //css3前缀
-    sourcemaps = require('gulp-sourcemaps'), //启用
-    browserSync = require('browser-sync'), //热更新
-    gulpIf = require('gulp-if'), //判断文件类型做相应动作
-    htmlmin = require('gulp-htmlmin'), //html压缩
-    cssmin = require('gulp-clean-css'), //css压缩
-    uglify = require('gulp-uglify'), //js压缩
-    imagemin = require('gulp-imagemin'), //图片压缩
-    fontmin = require('gulp-fontmin'), //字体压缩
-    rev = require('gulp-rev-append-all'), //MD5戳
-    cache = require('gulp-cache'), //缓存图片
-    del = require('del'), // del
-    runSequence = require('run-sequence'), //同步异步执行代码
-    notify = require("gulp-notify"), //消息通知
-    spritesmith = require('gulp.spritesmith'), //图片精灵
+//环境变量 dev开发环境 pro产品，默认开发
+const ENV = process.env.NODE_ENV
+const DEV = ENV === 'dev'
+const PRO = ENV === 'build'
 
-    buffer = require('vinyl-buffer'),
-    merge = require('merge-stream'), //合并
-    usemin = require('gulp-usemin'); //合并静态资源及路由
+const DEST = 'dist'
 
-/*--------------
- Path
- ---------------*/
-var _path = {
-    app: {
-        assets: 'app/assets',
-        views: 'app/views/',
-        scss: 'app/assets/scss/',
-        css: 'app/assets/css/',
-        js: 'app/assets/js/',
-        fonts: 'app/assets/fonts/',
-        imgs: 'app/assets/imgs/',
-        images: 'app/images/',
-        libs: 'app/libs/',
-        sprite: 'app/assets/imgs/sprite',
-        tpl: 'app/tpl/',
-    },
-    dist: {
-        assets: 'dist/assets/',
-        views: 'dist/views/',
-        css: 'dist/assets/css/',
-        fonts: 'dist/assets/fonts/',
-        imgs: 'dist/assets/imgs/',
-        images: 'dist/images/',
-        libs: 'dist/libs/',
-        js: 'dist/assets/js/'
+// 依赖
+const gulp = require("gulp"),
+    autoprefixer = require("gulp-autoprefixer"), //自动添加浏览器前缀
+    babel = require('gulp-babel'), // es6转es5
+    browserSync = require("browser-sync"), //文件热更新
+    cleanCss = require("gulp-clean-css"), //压缩css
+    concat = require("gulp-concat"), //合并js
+    connect = require("gulp-connect"), //本地服务器 自动刷新
+    del = require("del"), //删除文件
+    fileinclude = require("gulp-file-include"), //导入html公共部分
+    htmlMin = require("gulp-htmlmin"), //压缩html
+    imageMin = require("gulp-imagemin"), //压缩图片
+    less = require("gulp-less"), // 编译less
+    livereload = require('gulp-livereload'), //自动刷新，需配合浏览器插件livereload
+    notify = require("gulp-notify"), //通知消息
+    plumber = require("gulp-plumber"), //错误不终止watch
+    proxy = require("http-proxy-middleware"), //本地服务器代理
+    pump = require('pump'),
+    runSequence = require("run-sequence"), //同步执行gulp任务
+    sourcemaps = require('gulp-sourcemaps'), //启用sourcemaps
+    spritesmith = require("gulp.spritesmith"), //合并雪碧图
+    uglify = require("gulp-uglify") // 压缩js
+
+
+//删除dist下面的文件
+del.sync(['dist'])
+
+//压缩less
+gulp.task("cssMin", cb => {
+    if (ENV) {
+        pump([
+            gulp.src("src/less/main.less"),
+            sourcemaps.init(),
+            plumber({ //错误不终止并给出提示
+                errorHandler: notify.onError("Error: <%= error.message %>")
+            }),
+            less(),
+            autoprefixer({
+                browsers: ["last 5 versions"], //向下兼容到IE8
+                cascade: false //是否美化属性值 默认：true 
+            }),
+            sourcemaps.write('maps'),
+            gulp.dest("dist/css"),
+            connect.reload()
+        ], cb)
+    } else {
+        pump([
+            gulp.src("src/less/main.less"),
+            less(),
+            autoprefixer({
+                browsers: ["last 5 versions"], //向下兼容到IE8
+                cascade: false //是否美化属性值 默认：true 
+            }),
+            cleanCss({
+                advanced: false, //类型：Boolean 默认：true [是否开启高级优化（合并选择器等）]
+                compatibility: 'ie7', //保留ie7及以下兼容写法 
+                keepBreaks: true, //类型：Boolean 默认：false [是否保留换行]
+                keepSpecialComments: '*' //保留所有特殊前缀 当你用autoprefixer生成的浏览器前缀，如果不加这个参数，有可能将会删除你的部分前缀
+            }),
+            gulp.dest("dist/css")
+        ], cb)
     }
-};
+})
 
+//压缩JS
+gulp.task("jsMin", cb => {
+    // 手动按序执行js
+    let src = 'src/js/',
+        sequenceFiles = [
+            `${src}/utils.js`,
+            `${src}/api.js`,
+            `${src}/chart-config.js`,
+            `${src}/table-config.js`,
+            `${src}/common.js`
+        ]
 
-/*--------------
- Start browserSync server
- ---------------*/
-gulp.task('browserSync', () => {
-    browserSync({
-        server: {
-            baseDir: 'app/'
-        }
-    });
-});
+    if (DEV) {
+        pump([ //压缩编译公共模块
+            gulp.src(sequenceFiles),
+            sourcemaps.init(),
+            plumber({
+                //错误不终止并给出提示
+                errorHandler: notify.onError("Error: <%= error.message %>")
+            }),
+            concat('bundle.min.js'), //合并
+            sourcemaps.write('maps'),
+            gulp.dest('dist/js'),
+            connect.reload()
+        ])
 
+        pump([ //压缩业务模块
+            gulp.src('src/js/pages/*.js'),
+            sourcemaps.init(),
+            plumber({
+                //错误不终止并给出提示
+                errorHandler: notify.onError("Error: <%= error.message %>")
+            }),
+            sourcemaps.write('maps'),
+            gulp.dest('dist/js/pages'),
+            connect.reload()
+        ], cb)
+    } else {
+        pump([ //压缩编译公共模块
+            gulp.src(sequenceFiles),
+            babel({ presets: ['env'] }),
+            concat('bundle.min.js'), //合并
+            uglify(), //压缩操作
+            gulp.dest('dist/js')
+        ])
+        pump([ //压缩业务模块
+            gulp.src('src/js/pages/*'),
+            babel({ presets: ['env'] }),
+            uglify(),
+            gulp.dest('dist/js/pages')
+        ], cb)
+    }
 
-/*--------------
- Sass处理
- ---------------*/
-gulp.task('sass', ['clean:css'], () => {
-    return gulp.src(_path.app.scss + '**/*.scss') // 输入目录
-        .pipe(sourcemaps.init()) //启用sourcemaps
-        .pipe(sass().on('error', sass.logError)) // 将sass报错记录到日志
-        .pipe(autoprefixer({
-            browsers: 'last 2 versions'
-        }))
-        .pipe(sourcemaps.write('../maps'))
-        .pipe(browserSync.reload({ stream: true })) // 同步更新
-        .pipe(gulp.dest('app/assets/css/')) // 输出目录
-        .pipe(notify({ message: "SASS任务编译完成" }));
-});
+})
 
-/*--------------
- es6处理
- ---------------*/
-gulp.task('babel', () => {
-    gulp.src(_path.app.js + '**/*.js')
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(usemin({
-            js: [uglify()]
-        }))
-        .pipe(gulp.dest(_path.dist.js))
-        .pipe(notify({ message: "ES6转换ES5完成" })); //提示
-});
+//雪碧图 图片的名字为a.png 对应的类为.icon-a
+gulp.task("spritesmith", cb => {
+    pump([
+        gulp.src("src/img/sprite/*.png"),
+        spritesmith({
+            imgName: "img/sprite.png",
+            cssName: "less/sprite.less",
+            padding: 20,
+            algorithm: "binary-tree",
+            cssTemplate: "src/img/sprite/template.tpl"
+        }),
+        gulp.dest("src"),
+        connect.reload()
+    ], cb)
+})
 
+//压缩图片
+gulp.task('imageMin', ['cssMin'], cb => {
+    pump([
+        gulp.src('src/img/*.{png,jpg,gif,ico}'),
+        imageMin({
+            optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+            progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+            interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+            multipass: true //类型：Boolean 默认：false 多次优化svg直到完全优化
+        }),
+        gulp.dest('dist/img'),
+        connect.reload()
+    ], cb)
+})
 
-/*--------------
- Watchers
- ---------------*/
-gulp.task('watch', () => {
-    gulp.watch('app/' + '**/*.html', browserSync.reload);
-    gulp.watch(_path.app.scss + '**/*.scss', ['sass'], browserSync.reload);
-    gulp.watch(_path.app.js + '**/*.js', browserSync.reload);
-});
-
-/*--------------
- Merge Static
- ---------------*/
-gulp.task('usemin', () => {
-    return gulp.src(_path.app.views + '**/*.html')
-        .pipe(usemin({
-            js: [uglify()],
-            css: [cssmin()]
-        }))
-        .pipe(gulp.dest(_path.dist.views))
-        .pipe(notify({ message: "合并静态文件路由完成" })); //提示;
-});
-
-/*--------------
- Optimizing Html
- ---------------*/
-gulp.task('minifyHtml', () => {
-    var options = {
+//压缩html
+gulp.task("htmlMin", cb => {
+    const options = {
         removeComments: true, //清除HTML注释
         collapseWhitespace: true, //压缩HTML
-        collapseBooleanAttributes: true, //省略布尔属性的值 <input checked="true"/> ==> <input checked />
+        collapseBooleanAttributes: false, //省略布尔属性的值 <input checked="true"/> ==> <input />
         removeEmptyAttributes: true, //删除所有空格作属性值 <input id="" /> ==> <input />
-        removeScriptTypeAttributes: true, //删除<script>的type="text/javascript"
-        removeStyleLinkTypeAttributes: true, //删除<style>和<link>的type="text/css"
+        removeScriptTypeAttributes: false, //删除<script>的type="text/javascript"
+        removeStyleLinkTypeAttributes: false, //删除<style>和<link>的type="text/css"
         minifyJS: true, //压缩页面JS
         minifyCSS: true //压缩页面CSS
-    };
-    gulp.src('app/views/' + '**/*.html')
-        .pipe(htmlmin(options))
-        .pipe(rev())
-        .pipe(gulp.dest('dist/views'))
-        .pipe(notify({ message: 'html压缩完成' })); //提示成功
-});
+    }
+    if (DEV) {
+        pump([
+            gulp.src("dist/*.html"),
+            gulp.dest("dist"),
+            connect.reload()
+        ], cb)
+    } else {
+        pump([
+            gulp.src("dist/*.html"),
+            htmlMin(options),
+            gulp.dest("dist"),
+            connect.reload()
+        ], cb)
+    }
 
-/*--------------
- Optimizing Fonts
- ---------------*/
-gulp.task('minifyFont', () => {
-    gulp.src('app/assets/fonts' + '*.+(otf|eot|svg|ttf|woff)')
-        .pipe(fontmin({}))
-        .pipe(gulp.dest('dist/assets/fonts'))
-        .pipe(notify({
-            message: '字体压缩完成'
-        }));
-});
+})
 
+//迁移文件
+gulp.task("copyStatic", cb => {
+    pump([ //迁移第三方引用的库或插件
+        gulp.src("src/lib/**/*"),
+        gulp.dest("dist/lib")
+    ])
 
-/*--------------
- Optimizing Image
- ---------------*/
-gulp.task('minifyImg', () => {
-    gulp.src('app/assets/images' + '*.+(png|jpg|jpeg|gif|svg)')
-        .pipe(cache(imagemin({ //压缩未经压缩的图片
-            optimizationLevel: 5, // 默认：3  取值范围：0-7（优化等级）
-            progressive: true, // 默认：false 无损压缩jpg图片
-            interlaced: true, // 默认：false 隔行扫描gif进行渲染
-            multipass: true // 默认：false 多次优化svg直到完全优化
-        })))
-        .pipe(gulp.dest('dist/assets/images'))
-        .pipe(notify({ message: "图片压缩完成" })); //提示
-});
+    pump([ //迁移图片
+        gulp.src("src/img/*.{png,jpg,gif,ico}"),
+        gulp.dest("dist/img"),
+        connect.reload()
+    ], cb)
+})
 
-
-/*--------------
- Img Sprite
- ---------------*/
-gulp.task('sprite', () => {
-    var spriteData = gulp.src('app/assets/images/' + "sprite/**/*.+(png|jpg|jpeg|gif|svg)")
-        .pipe(spritesmith({
-            imgName: 'sprite.png',
-            cssName: '_sprite.scss',
-            padding: 10, // 图标之间的距离
-            algorithm: 'binary-tree', // 图标的排序方式
-            cssTemplate: _path.app.tpl + 'sprite/handlebarsInheritance.scss.handlebars' // 模板
-        }));
-
-    var imgStream = spriteData.img
-        .pipe(buffer())
-        .pipe(gulp.dest('app/assets/images/'))
-        .pipe(notify({ message: "icon合并完成" })); //提示
-
-    var cssStream = spriteData.css
-        .pipe(gulp.dest("app/assets/scss/sprite"))
-        .pipe(notify({ message: "sprite已生成mixin" })); //提示
-
-    return merge(imgStream, cssStream);
-});
-
-/*--------------
- MD5 Added
- ---------------*/
-gulp.task('rev', () => {
-    return gulp.src(_path.dist.views + "**/*.html")
-        .pipe(rev())
-        .pipe(gulp.dest(_path.dist.views))
-        .pipe(notify({
-            message: 'md5后缀添加成功'
-        }));
-});
+//导入html公共部分
+gulp.task("fileinclude", cb => {
+    pump([
+        gulp.src("src/**/*.html"),
+        fileinclude({
+            prefix: "@@",
+            basepath: "@file"
+        }),
+        gulp.dest("dist"),
+        connect.reload()
+    ], cb)
+})
 
 
-/*--------------
- Clone
- ---------------*/
-gulp.task('images:dist', () => { //前景图
-    return gulp.src(_path.app.images + '**/*')
-        .pipe(gulp.dest(_path.dist.images));
-});
-gulp.task('libs:dist', () => { //第三方库
-    return gulp.src(_path.app.libs + '**/*')
-        .pipe(gulp.dest(_path.dist.libs));
-});
+//  启动本地服务 并解决跨域
+gulp.task("server", () => {
+    connect.server({
+        host: '10.10.20.176', //本地host，默认为“loacalhost”
+        port: 8088, //端口
+        root: "../", //根指向
+        livereload: true, //自动刷新
+        middleware(connect, opt) { //中间件配置
+            return [
+                proxy('/webapi', { //代理配置
+                    target: 'http://10.10.40.33:8604', //跨域指向
+                    changeOrigin: true
+                })
+            ]
+        }
+    })
+})
 
 
-/*--------------
- Cleaning Something
- ---------------*/
+// 监听热更新
+gulp.task("watcher", () => {
+    gulp.watch("src/js/**/*", ["jsMin"])
+    gulp.watch("src/less/**/*", ["cssMin"])
+    gulp.watch("src/img/sprite/**/*", ["spritesmith"])
+    gulp.watch("src/**/*.html", ["fileinclude"])
+    gulp.watch("src/img/*.{png,jpg,gif}", ["copyImg"])
+})
 
-gulp.task('clean:dist', () => {
-    return del.sync(['dist/' + '**/*', '!dist/images']);
-});
-
-gulp.task('clean:css', () => {
-    return del.sync([_path.app.css + '**/*']);
-});
-
-
-// Build Sequences
-// --------------->
-gulp.task('build', () => {
+gulp.task("default", () => {
     runSequence(
-        'clean:dist',
-        'images:dist',
-        'libs:dist',
-        'usemin', ['minifyImg', 'minifyFont'],
-        'rev',
-        'minifyHtml'
-    );
-});
-
-// Default Sequences
-// --------------->
-gulp.task('default', () => {
-    runSequence(
-        'sprite', ['sass', 'babel', 'browserSync'],
-        'watch'
-    );
-});
+        "spritesmith", ["cssMin", "jsMin", "imageMin", "fileinclude", "copyStatic"],
+        "htmlMin", "watcher", "server"
+    )
+})
