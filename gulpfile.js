75ed5c1e193dfe446ced9778928b6a6f34024870
kpengWang <417940197@qq.com>
2018-03-31 16:25:19 +0800

    验证完include功能 以及md5后缀

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,8 @@ const PRO = ENV === "build"
 
 // 依赖
 const gulp = require("gulp"),
-    autoprefixer = require("gulp-autoprefixer"), //自动添加浏览器前缀
-    assign = require("lodash.assign"),
-    babelify = require("babelify"),
+    autoprefixer = require("gulp-autoprefixer"), //自动添加css浏览器前缀
+    babelify = require("babelify"), // es6转es5
     browserify = require("browserify"), // 模块化编译 
     buffer = require('vinyl-buffer'),
     cleanCss = require("gulp-clean-css"), //压缩css
@@ -23,10 +22,10 @@ const gulp = require("gulp"),
     plumber = require("gulp-plumber"), //错误不终止watch
     proxy = require("http-proxy-middleware"), //本地服务器代理
     pump = require("pump"),
+    rev = require('gulp-rev-append'), //添加版本号
     runSequence = require("run-sequence"), //同步执行gulp任务
-    rename = require("gulp-rename"),
     sass = require("gulp-sass"), // 编译sass
-    source = require("vinyl-source-stream"),
+    source = require("vinyl-source-stream"), //常规读取流，需要把读取流转换为 vinyl 文件对象,可以不再使用gulp-rename
     sourcemaps = require("gulp-sourcemaps"), //启用sourcemaps
     spritesmith = require("gulp.spritesmith"), //合并雪碧图
     uglify = require("gulp-uglify"),
@@ -75,49 +74,55 @@ gulp.task("es", cb => {
         entries: entriesFiles,
         debug: true
     }
-    const opts = assign({}, watchify.args, customOpts)
+    const opts = Object.assign({}, watchify.args, customOpts)
     const b = watchify(browserify(opts))
 
     return b.transform(babelify)
         .bundle()
         .on('error', () => {
             notify.onError("Error: <%= error.message %>")
-                // let args = Array.prototype.slice.call(arguments)
 
-            // //Send error to notification center with gulp-notify
+            // let args = Array.prototype.slice.call(arguments)
+
+            /* //Send error to notification center with gulp-notify*/
             // notify.onError({
             //     title: "Compile Error",
             //     message: "<%= error.message %>"
             // }).apply(this, args)
 
-            // //Keep gulp from hanging on this task
+            /* //Keep gulp from hanging on this task*/
             // this.emit('end')
         })
-
-    .pipe(source("bundle.js"))
-        .pipe(gulp.dest("./build/js"))
+        .pipe(source("bundle.js"))
+        .pipe(gulp.dest("build/js"))
         .pipe(connect.reload())
         .pipe(notify("es编译完成"))
+
 })
 
 //雪碧图 图片的名字为a.png 对应的类为.icon-a
 gulp.task("sprite", cb => {
-    const option = gulp.src("src/assets/img/sprite/**/*.*")
-        .pipe(spritesmith({
-            imgName: 'sprite.png', //生成的图片名
-            cssName: '_sprite.scss', //生成的css文件名
-            padding: 10, // 图标之间的距离
-            algorithm: 'binary-tree', // 图标的排序方式
-            cssTemplate: 'src/style/handlebarsInheritance.scss.handlebars' // 模板
-        }))
+
+    const option = {
+        imgName: 'sprite.png', //生成的图片名
+        cssName: '_sprite.scss', //生成的css文件名
+        padding: 10, // 图标之间的距离
+        algorithm: 'binary-tree', // 图标的排序方式
+        cssTemplate: 'src/style/handlebarsInheritance.scss.handlebars' // sprite输出模板
+    }
+
+    const spriteStream = pump([
+        gulp.src("src/assets/img/sprite/**/*.*"),
+        spritesmith(option)
+    ])
 
     // 合并图片
-    const imgStream = option.img
+    const imgStream = spriteStream.img
         .pipe(buffer()) //合并
         .pipe(gulp.dest("src/assets/img"))
 
     // 自动生成css
-    const cssStream = option.css
+    const cssStream = spriteStream.css
         .pipe(gulp.dest("src/style"))
         .pipe(connect.reload())
         .pipe(notify("图片合并完成"))
@@ -132,7 +137,6 @@ gulp.task("copyStatic", cb => {
         gulp.src("src/lib/**/*"),
         gulp.dest("build/lib")
     ])
-
 
     pump([
         //迁移字体
@@ -171,7 +175,7 @@ gulp.task("fileinclude", cb => {
             }),
             gulp.dest("build/view"),
             connect.reload(),
-            notify("html编译")
+            notify("html编译完成")
         ],
         cb
     )
@@ -204,13 +208,6 @@ gulp.task("watcher", () => {
     gulp.watch(["src/lib/**/*.*", "src/assets/img/**/*"], ["copyStatic"])
 })
 
-gulp.task("default", () => {
-    runSequence(
-        "sprite", ["sass", "es", "fileinclude", "copyStatic"],
-        "watcher",
-        "server"
-    )
-})
 
 /*
 ---------------------------------------
@@ -220,7 +217,7 @@ gulp.task("default", () => {
 
 //压缩html
 gulp.task("htmlMin", cb => {
-    const options = {
+    const option = {
         removeComments: true, //清除HTML注释
         collapseWhitespace: true, //压缩HTML
         collapseBooleanAttributes: false, //省略布尔属性的值 <input checked="true"/> ==> <input />
@@ -230,11 +227,17 @@ gulp.task("htmlMin", cb => {
         minifyJS: true, //压缩页面JS
         minifyCSS: true //压缩页面CSS
     }
-    pump([gulp.src("build/index.html"), htmlMin(options), gulp.dest("build")])
+    pump([
+        gulp.src("build/index.html"),
+        htmlMin(option),
+        rev(),
+        gulp.dest("build")
+    ])
     pump(
         [
             gulp.src("build/view/**/*.html"),
-            htmlMin(options),
+            htmlMin(option),
+            rev(),
             gulp.dest("build/view"),
             notify("html压缩完成")
         ],
@@ -244,7 +247,7 @@ gulp.task("htmlMin", cb => {
 
 // 压缩css
 gulp.task("cssMin", cb => {
-    const options = {
+    const option = {
         advanced: false, //类型：Boolean 默认：true [是否开启高级优化（合并选择器等）]
         compatibility: "ie7", //保留ie7及以下兼容写法
         keepBreaks: true, //类型：Boolean 默认：false [是否保留换行]
@@ -253,8 +256,8 @@ gulp.task("cssMin", cb => {
     pump(
         [
             gulp.src("build/css/main.css"),
-            cleanCss(options),
-            // rename({ suffix: ".min" }),
+            cleanCss(option),
+
             gulp.dest("build/css"),
             notify("css压缩完成")
         ],
@@ -268,7 +271,6 @@ gulp.task("jsMin", cb => {
         [
             gulp.src("build/js/**/*.js"),
             uglify(),
-            rename({ suffix: "min" }),
             gulp.dest("./build/js"),
             notify("js压缩完成")
         ],
@@ -294,6 +296,16 @@ gulp.task("imageMin", cb => {
     )
 })
 
-gulp.task("build", () => {
-    runSequence(["cssMin", "jsMin", "imageMin"], "htmlMin")
+
+
+gulp.task("default", () => {
+    if (DEV) {
+        runSequence(
+            "sprite", ["sass", "es", "fileinclude", "copyStatic"],
+            "watcher",
+            "server"
+        )
+    } else {
+        runSequence(["cssMin", "jsMin", "imageMin"], "htmlMin")
+    }
 })
