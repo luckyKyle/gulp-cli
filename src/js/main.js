d34487c8744ed3bbbdbf38936bcda2ad61cd29e3
xujun <417940197@qq.com>
2018-05-04 15:30:29 +0800

    补充Storage操作的方法

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,35 @@
-import { filterNonUnique } from './util/array'
+// import { filterNonUnique } from './util/array'
+import { setStorage, getStorage, hasStorage, removeStorage, clearStorage } from './util/storage'
 // import 'babel-polyfill'
 
 let a = { a1: '111', a2: 222 }
 let b = { b1: 333, b2: 444 }
 let c = Object.assign(a, b)
 
-console.log(filterNonUnique([31, 1232, 1142, 3, 4, 4, 5, 6, 7, 9, 8]))
-console.log(c)
+let btn1 = document.querySelector('#btn1') // 设置
+let btn2 = document.querySelector('#btn2') // 获取
+let btn3 = document.querySelector('#btn3') // 判断
+let btn4 = document.querySelector('#btn4') // 移除
+let btn5 = document.querySelector('#btn5') // 清空
+
+btn1.onclick = function() {
+    setStorage('test', c)
+}
+btn2.onclick = function() {
+    console.log(getStorage('test'))
+}
+
+btn3.onclick = function() {
+    console.log(hasStorage('test'))
+}
+
+btn4.onclick = function() {
+    console.log(removeStorage('test'))
+}
+
+btn5.onclick = function() {
+    console.log(clearStorage())
+}
+
+// console.log(filterNonUnique([31, 1232, 1142, 3, 4, 4, 5, 6, 7, 9, 8]))
+// console.log(c)
--- src/js/util/storage.js
+++ src/js/util/storage.js
@@ -1,3 +1,46 @@
 /***************************
 处理Storage相关的一些常规方法
 ****************************/
+
+/**
+ * 存入Storage
+ * @param key 设置指定key值
+ * @param val 设置指定value值
+ */
+export const setStorage = (key, val) => localStorage.setItem(key, JSON.stringify(val))
+
+/**
+ * 获取Storage
+ * @param key 指定key值
+ * @returns  {any}
+ */
+export const getStorage = (key) => {
+    let storageVal = localStorage.getItem(key)
+    storageVal = storageVal === 'undefined' ? '' : JSON.parse(storageVal)
+    return storageVal
+}
+
+/**
+ * 判断是否有对应key值的storage， 如果没有返回指定的key值则可以自定义默认值，不设置则返回布尔值
+ * @param key 指定key值
+ * @param defaultVal
+ * @returns Boolean
+ */
+export const hasStorage = (key, defaultVal) => {
+    if (!defaultVal) {
+        return !Object.is(getStorage(key), null)
+    }
+    setStorage(key, defaultVal)
+    return getStorage(key) ? getStorage(key) : defaultVal
+}
+
+/**
+ * 移除指定Storage
+ * @param key 移除指定key值
+ */
+export const removeStorage = (key) => localStorage.removeItem(key)
+
+/**
+ * 清空所有Storage数据
+ */
+export const clearStorage = () => localStorage.clear()
