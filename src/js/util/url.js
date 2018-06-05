259a6040091859186099f5e0cb8ab01119b4b62a
xujun <417940197@qq.com>
2018-06-05 19:45:19 +0800

    添加一个对象转url参数的方法

--- src/js/util/url.js
+++ src/js/util/url.js
@@ -29,3 +29,21 @@ export const getURLParameters = url =>
  * Example:redirect('https://google.com')
  */
 export const redirect = (url, asLink = true) => asLink ? (window.location.href = url) : (window.location.replace(url))
+
+/**
+ * Usage:  将一个对象转换为url的参数拼接
+ */
+export const objParseQuery = (param, key, encode) => {
+    if (param === null) return ''
+    let paramStr = ''
+    let t = typeof (param)
+    if (t === 'string' || t === 'number' || t === 'boolean') {
+        paramStr += '&' + key + '=' + ((encode == null || encode) ? encodeURIComponent(param) : param)
+    } else {
+        for (let i in param) {
+            let k = key === null ? i : key + (param instanceof Array ? `[${i}]` : `.${i}`)
+            paramStr += objParseQuery(param[i], k, encode)
+        }
+    }
+    return paramStr
+}
