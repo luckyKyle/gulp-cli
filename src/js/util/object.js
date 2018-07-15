9eb77e3537ff09cd5ea3861b9674f73858eed66e
kpengWang <417940197@qq.com>
2018-07-15 20:10:29 +0800

    变更：完善deepClone深拷贝方法

--- src/js/util/object.js
+++ src/js/util/object.js
@@ -26,6 +26,19 @@ export const equals = (a, b) => {
  * @returns
  */
 export const deepClone = obj => {
-  let proto = Object.getPrototypeOf(obj)
-  return Object.assign({}, Object.create(proto), obj)
+  if (!obj && typeof obj !== 'object') {
+    throw new Error('error arguments')
+  }
+  const targetObj = Array.isArray(obj) ? [] : {}
+  for (let key in obj) {
+    //只对对象自有属性进行拷贝
+    if (obj.hasOwnProperty(key)) {
+      if (obj[key] && typeof obj[key] === 'object') {
+        targetObj[key] = deepCopy(obj[key])
+      } else {
+        targetObj[key] = obj[key]
+      }
+    }
+  }
+  return targetObj
 }
