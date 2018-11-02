3526f825d2087f920993bfd7a890d712ceeca311
KyleWang <417940197@qq.com>
2018-11-02 11:27:41 +0800

    新增：使用window全局调用template方法

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,11 @@
 import * as a from './moudules/a'
 import * as api from './api'
-/* eslint-disable */
 const getOrder = () => {
   let container = $('#container')
   api.getNews().then(res => {
     const data = res.data
     console.log('a', a)
-    console.log(data.order)
+    container.html(window.template('tplOrdertList', data.order))
   })
 }
 getOrder()
